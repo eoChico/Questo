50db98ca5fccd2d5c70246de1de8858162b96c88
Francisco Arthur <francisarthfernandes@gmail.com>
2024-03-18 18:28:11 -0300

    login obrigatório

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required
 def profile(request):
     return render(request, 'profile.html')
+
+@login_required
+def calendar(request):
+    return render(request, 'calendar.html')
